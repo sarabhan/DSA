a4c2820594442baebfa599c99a2ce4a0f08ee2ce
Sarvika Bhan <sarvikab2003@gmail.com>
2026-02-27 10:46:53 +0530

    added inverse diamond

---
+++ pattern_problems/inverse_diamond.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n; cin>>n;
+    for(int i=0; i<2*n; i++)
+    {
+        if(i<n)
+        {
+            for(int j=0; j<=n-1-i; j++)
+            {
+                cout<<"*";
+            }
+            for(int j=n-i; j<n+i; j++)
+            {
+                cout<<" ";
+            }
+            for(int j=n+i; j<2*n; j++)
+            {
+                cout<<"*";
+            }
+        }
+        else
+        {
+            for(int j=0; j<=i-n; j++)
+            {
+                cout<<"*";
+            }
+            for(int j=i-n+1; j<2*n-i+n-1; j++)
+            {
+                cout<<" ";
+            }
+            for(int j=2*n-i+n-1; j<2*n; j++)
+            {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+}
