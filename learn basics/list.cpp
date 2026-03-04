57ecb1220335b5978bf868dc73a70691493b5dd1
Sarvika Bhan <sarvikab2003@gmail.com>
2026-03-04 16:03:15 +0530

    updated list.cpp

--- learn basics/list.cpp
+++ learn basics/list.cpp
@@ -7,6 +7,10 @@ int main()
     l.push_back(2); //slower
     l.emplace_back(3); //faster
 
-    
-    l.push_front(100); 
+    //vectors are costly for insertion as target index has to be reached by
+    //incrementing the 1st index
+    l.push_front(100); //however, list has a separate function to add an element in the start
+    l.emplace_front(200); //obviously faster
+
+    //REST ALL OTHER FUNCTIONS ARE SAMEE
 }
