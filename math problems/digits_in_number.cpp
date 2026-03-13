5fcf48eaf6dba1ebdcdeeb9039e4b909100f98e1
Sarvika Bhan <sarvikab2003@gmail.com>
2026-03-13 10:49:45 +0530

    more optimized apprach for digits_of_number

--- math problems/digits_in_number.cpp
+++ math problems/digits_in_number.cpp
@@ -4,9 +4,17 @@ int main()
 {
     int n;
     cin>>n;
-    while(n>0)
-    {
-        cout<<n%10<<",";
-        n=n/10;
-    }
+    // int count=0;
+    // while(n>0)
+    // {
+    //     // cout<<n%10<<",";
+    //     n=n/10;
+    //     count++;
+    // }
+    // cout<<count;
+    //however this is bruteforce approach. its time complexity is O(log10(n)) there is another way that uses less space and time.
+    
+    //time complexity of this is 0(1) 
+    cout<<int(log10(n)+1);
+    
 }
