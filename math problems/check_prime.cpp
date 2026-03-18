6c5de0689f99a18ba677ff776a01914f6d78b664
Sarvika Bhan <sarvikab2003@gmail.com>
2026-03-18 15:35:12 +0530

    added check_prime.cpp

---
+++ math problems/check_prime.cpp
@@ -0,0 +1,18 @@
+#include<iostream> 
+using namespace std; 
+int main() 
+{ 
+    int n, rem; 
+    cin>>n; 
+    for(int i=2; i<=int(sqrt(n)); i++) 
+    { 
+        rem=n%i; 
+        if(rem==0) 
+        { 
+            cout<<"not a prime"; 
+            break; 
+        } 
+    } 
+    if(rem!=0) cout<<"yes a prime"; 
+    return 0; 
+}
