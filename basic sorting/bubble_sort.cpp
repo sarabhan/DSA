6513330adb1ae3bce2d26182b018346624e39fbe
Sarvika Bhan <sarvikab2003@gmail.com>
2026-04-09 15:23:54 +0530

    added bubble sort

---
+++ basic sorting/bubble_sort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void bubble_sort(vector<int> &v, int size)
+{
+    for(int i=0; i<size-1; i++)
+    {
+        for(int j=0; j<size-i-1; j++)
+        {
+            if(v[j]>v[j+1])
+            {
+                int temp;
+                temp = v[j];
+                v[j] = v[j+1];
+                v[j+1] = temp;
+                
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int size; 
+    cout<<"enter vector size: ";
+    cin>>size;
+    vector<int> v;
+    for(int i=0; i<size; i++)
+    {
+        int a; cin>>a;
+        v.emplace_back(a);
+    }
+    bubble_sort(v, size);
+    for(auto i:v)
+    {
+        cout<<i<<",";
+    }
+}
