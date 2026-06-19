cfa0fcd733c9ac27a926cbf7d0d65b93ed3bacab
Sarvika Bhan <sarvikab2003@gmail.com>
2026-06-19 17:19:35 +0530

    more questions

---
+++ hard/count_reverse_pair.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int count_reverse_pairs(vector<int> &v, int low, int mid, int high)
+{
+    vector<int> vtemp;
+    int i=low, j = mid+1; 
+
+    while(i<=mid && j<=high)
+    
+
+}
+int main()
+{
+    vector<int> v = {3,1,2,4,1,5,2,6,4};
+    return 0;
+}
