865379d1be28809bb38cb4f7456157df1a549430
Sarvika Bhan <sarvikab2003@gmail.com>
2026-07-10 17:33:55 +0530

    solved more quesitons

--- BS on 1D vector/search_in_rotated_sorted_arr.cpp
+++ BS on 1D vector/search_in_rotated_sorted_arr.cpp
@@ -8,14 +8,28 @@ int search(vector<int> &v, int size, int low, int high, int num)
     {
         int mid = low+(high-low)/2;
         if(v[mid]==num)
-            loc = mid;
-        else if(v[mid])
+            return mid;
+
+        if(v[low]<=v[mid])
+        {
+            if(num>=v[low] && num<v[mid])
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        else
+        {
+            if(num<=v[high] && num>v[mid])
+                low = mid+1;
+            else
+                high = mid-1;
+        }
     }
-    return loc;
+    return -1;
 }
 int main() 
 {
-    vector<int> v = {3, 4, 13, 13, 13, 20, 40};
-    cout<<search(v, v.size(),0,v.size()-1, 13);
+    vector<int> v = {4, 5, 6, 7, 0, 1, 2};
+    cout<<search(v, v.size(),0,v.size()-1, 0);
     return 0;   
 }
